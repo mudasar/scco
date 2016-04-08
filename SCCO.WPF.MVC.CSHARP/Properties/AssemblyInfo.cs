fd2e1b1c23a708a4b8b4dc067991c10910551ee4
Jeralane <tbki_jea@yahoo.com>
2016-04-08 08:14:06 +0800

    Released v2.3.0.4

--- SCCO.WPF.MVC.CSHARP/Properties/AssemblyInfo.cs
+++ SCCO.WPF.MVC.CSHARP/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyVersion("2.0.0.0")]
 // [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyVersion("2.3.0.2")]
-[assembly: AssemblyFileVersionAttribute("2.3.0.2")]
+[assembly: AssemblyVersion("2.3.0.4")]
+[assembly: AssemblyFileVersionAttribute("2.3.0.4")]
